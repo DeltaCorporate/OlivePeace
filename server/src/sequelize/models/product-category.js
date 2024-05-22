101ac34ce7ede2daa532f69ff4a33de6562f4bd2
Ahmindache Jawad <jawad.ahmindache@gmail.com>
2024-05-22 16:19:30 +0200

    Model ProductCategory mongoose

--- server/src/sequelize/models/product-category.js
+++ server/src/sequelize/models/product-category.js
@@ -1,6 +1,7 @@
-import {Model, DataTypes, NOW} from 'sequelize';
+import { Model, DataTypes, NOW } from 'sequelize';
 import db from './index.js';
-import Promotion from "./promotion.js";
+import Promotion from './promotion.js';
+import ProductCategoryMongoose from '../../mongoose/models/product-category.model.js'; // Import du modèle Mongoose
 
 class ProductCategory extends Model {}
 
@@ -23,8 +24,51 @@ ProductCategory.init({
 }, {
     sequelize: db.sequelize,
     modelName: 'ProductCategory',
-    underscored: true
+    underscored: true,
+    hooks: {
+        afterCreate: async (category, options) => {
+            try {
+                await ProductCategoryMongoose.create({
+                    productCategoryId: category.id,
+                    name: category.name,
+                    imageName: category.imageName,
+                    description: category.description,
+                    slug: category.slug,
+                    createdAt: category.createdAt,
+                    updatedAt: category.updatedAt,
+                    promotion_id: category.promotion_id
+                });
+            } catch (error) {
+                console.error('Failed to create category in MongoDB:', error);
+            }
+        },
+        afterUpdate: async (category, options) => {
+            try {
+                await ProductCategoryMongoose.findOneAndUpdate(
+                    { productCategoryId: category.id },
+                    {
+                        name: category.name,
+                        imageName: category.imageName,
+                        description: category.description,
+                        slug: category.slug,
+                        updatedAt: category.updatedAt,
+                        promotion_id: category.promotion_id
+                    }
+                );
+            } catch (error) {
+                console.error('Failed to update category in MongoDB:', error);
+            }
+        },
+        afterDestroy: async (category, options) => {
+            try {
+                await ProductCategoryMongoose.findOneAndDelete({ productCategoryId: category.id });
+            } catch (error) {
+                console.error('Failed to delete category in MongoDB:', error);
+            }
+        }
+    }
 });
+
 ProductCategory.belongsTo(Promotion, { foreignKey: 'promotion_id' });
 
 export default ProductCategory;
